061a45ad0bd44f3c53bee39978bb653839deec33
Joan Pedro <jpos.senai@gmail.com>
2020-10-01 14:53:04 -0300

    jpa - object on detachement state

--- Exercicios-JPA/src/modelo/basico/AlteraUsuario3.java
+++ Exercicios-JPA/src/modelo/basico/AlteraUsuario3.java
@@ -4,17 +4,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class AlterarUsuario2 {
+public class AlteraUsuario3 {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
 		EntityManager em = emf.createEntityManager();
 
-		IUsuario usuario = em.find(Usuario.class, 7L);
-		usuario.setNome("JoanPw");
-		usuario.setEmail("jw@email.com");
-
 		em.getTransaction().begin();
-//		em.merge(usuario);
+		
+		IUsuario usuario = em.find(Usuario.class, 7L);
+
+		usuario.setNome("JoanPwdd");
+		usuario.setEmail("jwdd@email.com");
+
+		em.detach(usuario);
+		em.merge(usuario);
 
 		em.getTransaction().commit();
 
