8036f63c9115661b138b8f796a0150606a36b4c2
Joan Pedro <jpos.senai@gmail.com>
2020-10-02 20:28:01 -0300

    springboot http req params and queryParams

---
+++ alias-springboot/src/main/java/br/com/cod3r/reviewspringboot/controllers/CalculadoraController.java
@@ -0,0 +1,27 @@
+package br.com.cod3r.reviewspringboot.controllers;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculadora")
+public class CalculadoraController {
+
+	@GetMapping("somar/{a}/{b}")
+	public Double somar(@PathVariable double a, @PathVariable double b) {
+		return a + b;
+	}
+	
+	@GetMapping("/subtrair")
+	public Double subtrair(
+		@RequestParam(name = "a") double a,
+		@RequestParam(name = "b") double b
+	) {
+		return a - b;
+	}
+}
--- alias-springboot/src/main/java/br/com/cod3r/reviewspringboot/controllers/ClienteController.java
+++ alias-springboot/src/main/java/br/com/cod3r/reviewspringboot/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.cod3r.reviewspringboot.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.reviewspringboot.models.Cliente;
@@ -19,5 +20,12 @@ public class ClienteController {
 	@GetMapping("/{id}")
 	public Cliente obterClientePorId(@PathVariable int id) {
 		return new Cliente(id, "Maria", "987.654.321-00");
+	}
+	
+	@GetMapping("/cli")
+	public Cliente obterClientePorQuery(
+		@RequestParam(name = "id", defaultValue = "1", required = true) int id
+	) {
+		return new Cliente(id, "João Algusto", "111.222.333-44");
 	}
 }
